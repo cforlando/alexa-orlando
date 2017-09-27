a5f7fb489026239b52cb6fbdbdfb327f3b14fc15
akozlik <akozlik@gmail.com>
2017-09-26 23:48:30 -0400

    Working on adding a few intents

---
+++ index.js
@@ -0,0 +1,37 @@
+var Alexa = require('alexa-sdk');
+
+var handlers = {
+    'GetDistrictIntent': function() {
+        this.emit(':tell', "This is not implemented yet!");
+    },
+    'GetNextCityCouncilMeetingIntent': function() {
+
+    },
+    'GetLocationOfCityCouncilMeetingIntent': function() {
+        this.emit(':tell', "City Council meetings are in held in Council Chamber, 2nd Floor, City Hall, 400 S. Orange Avenue.  For additional information, please contact the City Clerk’s Office, 407.246.2251.");
+    },
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = HELP_MESSAGE;
+        var reprompt = HELP_REPROMPT;
+
+        this.response.speak(speechOutput).listen(reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+    'AMAZON.StopIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    }
+};
+
+exports.handler = function (event, context, callback) {
+    var alexa = Alexa.handler(event, context, callback);
+    alexa.registerHandlers(handlers);
+    console.log("Request: " + event.request);
+    console.log("Locale " + event.request.locale);
+    print("Hi");
+    alexa.execute();
+};
