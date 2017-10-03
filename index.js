9f08a7773a6aea6e4bb48ec0ab26b8a2c5c3b79e
akozlik <akozlik@gmail.com>
2017-10-02 22:39:08 -0400

    Cleaned up code

--- index.js
+++ index.js
@@ -34,15 +34,17 @@ var handlers = {
     'GetLocationOfCityCouncilMeetingIntent': function() {
         this.emit(':tell', "City Council meetings are held in Council Chamber, 2nd Floor, City Hall, 400 S. Orange Avenue.  For additional information, please contact the City Clerk’s Office, 407.246.2251.");
     },
-    'GetCityCommissioner': function() { 
-        var address = '111 w jefferson st Orlando, Fl 32801';
+    'GetCityCommissioner': function() {
+
+        var address = this.event.request.intent.slots.address.value;
+
         //get ward
         // function GetCommissioner(ward) {}
         var that = this;
         getWard(address, function(ward) {
             getCommissioner(ward, function(commissioner) {
                 console.log("Commissioner: " + commissioner);
-                that.emit(':tell', commissioner); 
+                that.emit(':tell', commissioner);
             });
         });
     },
@@ -112,9 +114,9 @@ function getPhoneNumberForDepartment(department) {
 
 function GetDayInSec() {
     //Takes the current date, removes the time, and then returns the seconds
-	var tDate = new Date()
-	var tDate =  (tDate.getMonth() +1)+ "/" + tDate.getDate() + "/" + tDate.getFullYear();
-	
+	var tDate = new Date();
+	tDate =  (tDate.getMonth() +1)+ "/" + tDate.getDate() + "/" + tDate.getFullYear();
+
 	sDateInSec = Date.parse(tDate);
     return sDateInSec;
 }
@@ -124,20 +126,22 @@ function NextDate(aDates, CurrentDate) {
     //aDates must be ordered or this fails
     var nLength = aDates.length;
 	var i = 0;
-	while (i < nLength) { 
-    	sCurr = Date.parse(aDates[i]);
-    	if (sCurr > CurrentDate)  {
-    		sNext = aDates[i];
-    		break;
-    		}
-    	i++;
+	while (i < nLength) {
+        sCurr = Date.parse(aDates[i]);
+        if (sCurr > CurrentDate)  {
+            sNext = aDates[i];
+            break;
+        }
+
+        i++;
 	}
+
     return sNext;
 }
 
 function getRandomOrlandoFunFact() {
     var randomIndex = Math.floor(Math.random() * FunFacts.facts.length);
-    return FunFacts.facts[randomIndex]
+    return FunFacts.facts[randomIndex];
 }
 
 function getCrimeReport(callback) {
@@ -156,12 +160,12 @@ function getCrimeReport(callback) {
                 var crimeString = "There was a " + crimeData.DESC + " reported at " + crimeData.LOCATION;
                 callback(crimeString);
             }
-         })
+         });
     })
     .catch( function(error) {
         console.log("axios Error:" + error);
         callback("There was a problem getting the data.");
-    })
+    });
 }
 
 function getEventParking(date, callback) {
@@ -184,72 +188,74 @@ function getEventParking(date, callback) {
         }
 
         callback(hasEvent);
-    })
+    });
 }
 
-function getWard(address, callback) {   
+function getWard(address, callback) {
     // global city_Ward variable to access data
-    var city_Ward; 
-    var baseApiUrl = 'https://alpha.orlando.gov/OCServiceHandler.axd?url=ocsvc/public/spatial/findaddress&address=';  
-    // encode uri for the baseApiUrl 
-    address = encodeURIComponent(address); 
+    var city_Ward;
+    var baseApiUrl = 'https://alpha.orlando.gov/OCServiceHandler.axd?url=ocsvc/public/spatial/findaddress&address=';
+    // encode uri for the baseApiUrl
+    address = encodeURIComponent(address);
 
     var apiURL = baseApiUrl + address;
     axios.get(apiURL)
         .then(function(response) {
             // check for error
-            if (response.status !== 200) { response.status; return; }
-            //check if data response came through 
-            //loop through data to find the District Ward. 
+            if (response.status !== 200) {
+                return;
+            }
+            //check if data response came through
+            //loop through data to find the District Ward.
             // the data json should look like this -> { locations: [{}], abc: '', xyz: '' }
             // city_Ward ->>>> 'Ward': 'some number'
             for (var i=0; i<response.data.locations.length; i++) {
-                var location = response.data.locations[i]; 
+                var location = response.data.locations[i];
                 city_Ward = location.Ward;
                 callback(city_Ward);
             }
         })
         .catch(function(err) {
             console.log("Error: " + err);
-        })
+        });
 }
 
 function getCommissioner(ward, callback) {
     console.log("Getting commissioner");
-    var commissioner; 
+    var commissioner;
     var baseApiUrl = 'https://alpha.orlando.gov/OCServiceHandler.axd?url=ocsvc/Public/InMyNeighbourhood/Councillors&Ward=';
     //encode uri for ward
-    ward = encodeURIComponent(ward); 
-    //final api url 
-    var apiURL = baseApiUrl + ward; 
+    ward = encodeURIComponent(ward);
+    //final api url
+    var apiURL = baseApiUrl + ward;
     console.log("API URL: " + apiURL);
     axios.get(apiURL)
         .then(function(response) {
             // check for error
             // if (response.data !== 200) { console.log("Error Response"); response.status; return; }
             // extract { response.data.responseContent } -> long html string
-            // regex and find the text in between the 2nd instance of { <h3> _ </h3> } 
+            // regex and find the text in between the 2nd instance of { <h3> _ </h3> }
             console.log("Response Content: " + response.data.responseContent);
-            var commissioner = findCommissionerString(response.data.responseContent); // returns commissioner  
-            callback(commissioner); 
-        })
+            var commissioner = findCommissionerString(response.data.responseContent); // returns commissioner
+            callback(commissioner);
+        });
 }
 
 function findCommissionerString(dataString) {
-    var delimiter = 'h3', 
-    start = 2, 
+    var delimiter = 'h3',
+    start = 2,
     //splits the first 2 <h3> instances
-    tokens = dataString.split(delimiter).slice(start), 
-    half_str = tokens.join(delimiter); 
+    tokens = dataString.split(delimiter).slice(start),
+    half_str = tokens.join(delimiter);
 
-    var beg_h3 = half_str.indexOf('h3', 0); 
-    var end_h3 = half_str.indexOf('h3', (beg_h3 + 1)); 
-    var h3_str = half_str.slice(beg_h3 -1, end_h3 + 3); 
+    var beg_h3 = half_str.indexOf('h3', 0);
+    var end_h3 = half_str.indexOf('h3', (beg_h3 + 1));
+    var h3_str = half_str.slice(beg_h3 -1, end_h3 + 3);
 
-    brack_1 = h3_str.indexOf('>', 1); 
-    brack_2 = h3_str.indexOf('<', brack_1); 
+    brack_1 = h3_str.indexOf('>', 1);
+    brack_2 = h3_str.indexOf('<', brack_1);
 
-    var final_str = h3_str.slice(brack_1 + 1, brack_2); 
+    var final_str = h3_str.slice(brack_1 + 1, brack_2);
 
-    return commissioner = final_str;
+    return final_str;
 }
